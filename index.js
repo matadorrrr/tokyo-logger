8750ead428cb7067484542c388a7f46613e90d53
unknown <tmatsumoto@W-0215.dit.co.jp>
2015-08-10 11:22:33 +0900

    some fix

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
 var __tokyoDateTime = require('./lib/tokyo-datetime.js'),
     __fs = require('fs'),
-    __stream = require('stream'),
+    __emitter = require('events').EventEmitter,
     __util = require('util');
 	
 module.exports = (function(filePath){
-    var logger =function(config){
-        var config_ = null, 
-            argType = typeof config;
-			
-        try{ config_ = config.get('tokyo-logger'); } catch(e){ }
+
+  var logger = function(config){
+    var logConfig = null;
+    try{ logConfig = config.get('tokyo-logger'); } catch(e){ }
         
-        this.readable = true;
-        this.dateTimeFormat = 'YYYY/MM/DD HH:mm:ss.SSS';
-		
-        if(config_ === null && argType === 'undefined'){
-            this.stdout = true;
-        }
-        else if(config_){
-            this.fileName = config_.fileName;
-            this.stdout = config_.stdout;
-            if(config_.dateTimeFormat){
-                this.dateTimeFormat = config_.dateTimeFormat;
-            }
-        }
-        else if(argType === 'string'){
-            this.fileName = config;
-            this.stdout = false;
-        }
-        else{
-            throw new TypeError('wrong arugment');
-        }		
-    };
-	
-    __util.inherits(logger, __stream.Stream);
-	
-    logger.prototype.write = function(tag, message){
-        var dateTime = __tokyoDateTime.get(this.dateTimeFormat);
-        var logMsg = makeLogMessage(dateTime, tag, message);
-        if(this.fileName){
-            __fs.appendFile(this.fileName, logMsg, function(error){
-                if(error) throw error;
-            });
-        }
-        if(this.stdout){
-            process.stdout.write(logMsg);
-        }
-        this.emit(tag, message, dateTime);
-    };
-	
-    function makeLogMessage(dateTime, tag, message) {
-        return dateTime +' :: [' + tag + '] :: ' + message + '\n';
+    this.dateTimeFormat = 'YYYY/MM/DD HH:mm:ss.SSS';
+        
+    if((typeof logConfig) === 'Object'){
+        if(logConfig.fileName){this.fileName = logConfig.fileName;}
+        if(logConfig.stdout){this.stdout = logConfig.stdout;}
+        if(logConfig.dateTimeFormat){this.dateTimeFormat = logConfig.dateTimeFormat;}
     }
+    else if((typeof config) === "string"){
+        this.fileName = config;
+        this.stdout = false;
+    }
+    else if(config === undefined){
+        this.stdout = true;
+    }
+    else{
+        throw new Error('wrong argument');
+    }
+  };
 	
-    return logger;
+  __util.inherits(logger, __emitter);
+	
+  logger.prototype.write = function(tag, message){
+    var dateTime = __tokyoDateTime.get(this.dateTimeFormat);
+    var logMsg = makeLogMessage(dateTime, tag, message);
+    if(this.fileName){
+      __fs.appendFile(this.fileName, logMsg, function(error){
+        if(error) throw error;
+      });
+    }
+    if(this.stdout){
+      process.stdout.write(logMsg);
+    }
+    this.emit(tag, message, dateTime);
+  };
+	
+  function makeLogMessage(dateTime, tag, message) {
+    return dateTime +' :: [' + tag + '] :: ' + message + '\n';
+  }
+	
+  return logger;
 })();
--- test.js
+++ test.js
@@ -1,6 +1,4 @@
-var tokyoLogger = require('./index.js');
-
-var logger = new tokyoLogger('./test.log');
+var logger = new (require('./index.js'))('./test.log');
 
 logger.on('info', function(message, dateTime){
 	console.log('received info message "' + message + '" on ' + dateTime);
